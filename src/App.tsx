5ba176b1ceacc44da079caa977c2aa941419c12e
Vladimir K <vladimir.sdev@gmail.com>
2020-09-25 21:18:45 +0300

    Add paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 
 import TasksList from "./TasksList";
 import AddTask from "./AddTask";
 
 import "./styles.css";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      "& > *": {
+        margin: theme.spacing(1),
+        width: theme.spacing(60)
+      }
+    }
+  })
+);
+
 export default function App() {
   const [tasks, setTasks] = React.useState<Array<string>>(["Create React App"]);
+  const classes = useStyles();
 
   const addTask = (taskName: string) => {
-    setTasks([...tasks, taskName]);
+    if (taskName) {
+      setTasks([...tasks, taskName]);
+    }
   };
 
   const deleteTask = (taskName: string) => {
@@ -18,9 +37,13 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Task List</h1>
+      <h1>ToDo List</h1>
       <AddTask handleAddTask={addTask} />
-      <TasksList tasks={tasks} handleDeleteTask={deleteTask} />
+      <div className={classes.root}>
+        <Paper elevation={5}>
+          <TasksList tasks={tasks} handleDeleteTask={deleteTask} />
+        </Paper>
+      </div>
     </div>
   );
 }
